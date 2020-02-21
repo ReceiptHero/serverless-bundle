057a1a3acb30842b11ff5eb3bbcf5f9443b69734
Tuomas Peippo <tuomas@peippo.eu>
2020-02-21 14:01:00 +0200

    Add twistlock to externals

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -176,7 +176,7 @@ module.exports = ignoreWarmupPlugin({
   stats: ENABLE_STATS ? "normal" : "errors-only",
   devtool: ENABLE_SOURCE_MAPS ? "source-map" : false,
   // Exclude "aws-sdk" since it's a built-in package
-  externals: ["aws-sdk", "knex", "sharp"],
+  externals: ["aws-sdk", "knex", "sharp", "twistlock"],
   mode: isLocal ? "development" : "production",
   performance: {
     // Turn off size warnings for entry points

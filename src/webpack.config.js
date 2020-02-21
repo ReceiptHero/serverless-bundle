c9f96f0f7d44bc692249a390c3893cbe5aa1165f
Tuomas Peippo <tuomas@peippo.eu>
2020-02-21 15:07:47 +0200

    test

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -176,7 +176,12 @@ module.exports = ignoreWarmupPlugin({
   stats: ENABLE_STATS ? "normal" : "errors-only",
   devtool: ENABLE_SOURCE_MAPS ? "source-map" : false,
   // Exclude "aws-sdk" since it's a built-in package
-  externals: ["aws-sdk", "knex", "sharp", { twistlock: "commonjs twistlock" }],
+  externals: [
+    "aws-sdk",
+    "knex",
+    "sharp",
+    { "/opt/twistlock": "commonjs /opt/twistlock" }
+  ],
   mode: isLocal ? "development" : "production",
   performance: {
     // Turn off size warnings for entry points
